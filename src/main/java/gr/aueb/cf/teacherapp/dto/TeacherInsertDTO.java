529a234b1e73b80563b4fa86513e84f8e1de1645
Katerina Savidaki <ksavvidaki@gmail.com>
2025-05-29 19:12:06 +0300

    feat: teacher insert dto added

---
+++ src/main/java/gr/aueb/cf/teacherapp/dto/TeacherInsertDTO.java
@@ -0,0 +1,22 @@
+package gr.aueb.cf.teacherapp.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class TeacherInsertDTO {
+
+    @NotNull(message = "Firstname must not be empty")
+    @Size(min = 2, message = "Firstname must contain at least 2 characters")
+    private String firstname;
+
+    @NotNull(message = "Lastname must not be empty")
+    @Size(min = 2, message = "Lastname must contain at least 2 characters")
+    private String lastname;
+
+    @Pattern(regexp = "\\d{9,}", message = "Vat can not be less than 9 characters")
+    private String vat;
+
+    @NotNull(message = "Region must not be empty")
+    private Long regionId;
+}
