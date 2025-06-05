2c971431a88d78ac6888e9931063a7fbb7bde04c
Katerina Savidaki <ksavvidaki@gmail.com>
2025-06-05 18:17:05 +0300

    fix: student insert dto added in method in advance of student entity

--- src/main/java/gr/aueb/cf/teacherapp/service/IStudentService.java
+++ src/main/java/gr/aueb/cf/teacherapp/service/IStudentService.java
@@ -3,13 +3,14 @@ package gr.aueb.cf.teacherapp.service;
 import gr.aueb.cf.teacherapp.core.exceptions.EntityAlreadyExistsException;
 import gr.aueb.cf.teacherapp.core.exceptions.EntityInvalidArgumentException;
 import gr.aueb.cf.teacherapp.core.exceptions.EntityNotFoundException;
+import gr.aueb.cf.teacherapp.dto.StudentInsertDTO;
 import gr.aueb.cf.teacherapp.dto.StudentReadOnlyDTO;
 import gr.aueb.cf.teacherapp.model.Student;
 import org.springframework.data.domain.Page;
 
 public interface IStudentService {
 
-    Student saveStudent(Student student)
+    Student saveStudent(StudentInsertDTO studentInsertDTO)
             throws EntityAlreadyExistsException, EntityInvalidArgumentException;
     Student getStudentById(Long id)
             throws EntityInvalidArgumentException, EntityNotFoundException;
